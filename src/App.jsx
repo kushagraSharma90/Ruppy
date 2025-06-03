32ccd43bd482233fb0795494ed2f8995aa583b69
mishrapriyanshu0103 <mishrapriyanshu0103>
2025-06-03 23:46:44 +0530

    Fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Navbar from "./components/home/Navbar"
-import Footer from "./components/home/Footer"
-import Home from "./pages/Home"
-import OurStory from "./pages/OurStory"
-import Loans from "./pages/Loans"
-import Calculators from "./pages/Calculators"
-import BecomePartner from "./pages/BecomePartner"
-import ContactButtons from "./components/home/ContactButtons"
-import "./index.css"
-import PersonalLoan from "./pages/PersonalLoan"
-import UsedCarLoan from "./pages/UsedCarLoan"
-import NewCarLoan from "./pages/NewCarLoan"
-import LoanAgainstCar from "./pages/LoanAgainstCar"
-import AboutUs from "./pages/AboutUs"
-import OurMission from "./pages/OurMission"
-import ApplyButton from "./pages/ApplyButton"
-import ScrollToTop from "./components/ScrollToTop" // ✅ Add this import
-  import BlogDetail from "./components/home/BlogDetail"
-import ContactUs from "./pages/ContactUs"
-import Community from "./pages/Community"
-import Career from "./pages/community/career"
-import OurCoreValues from "./pages/community/our-core-values"
-import WorkCulture from "./pages/community/work-culture"
-import FAQ from "./pages/community/faq"
-import Blogs from "./components/home/Blogs"
-import PrivacyPolicy from "./components/PrivacyPolicy"  
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/home/Navbar";
+import Footer from "./components/home/Footer";
+import Home from "./pages/Home";
+import OurStory from "./pages/OurStory";
+import Loans from "./pages/Loans";
+import Calculators from "./pages/Calculators";
+import BecomePartner from "./pages/BecomePartner";
+import ContactButtons from "./components/home/ContactButtons";
+import "./index.css";
+import PersonalLoan from "./pages/PersonalLoan";
+import UsedCarLoan from "./pages/UsedCarLoan";
+import NewCarLoan from "./pages/NewCarLoan";
+import LoanAgainstCar from "./pages/LoanAgainstCar";
+import AboutUs from "./pages/AboutUs";
+import OurMission from "./pages/OurMission";
+import ApplyButton from "./pages/ApplyButton";
+import ScrollToTop from "./components/ScrollToTop"; // ✅ Add this import
+import BlogDetail from "./components/home/BlogDetail";
+import ContactUs from "./pages/ContactUs";
+import Community from "./pages/Community";
+import Career from "./pages/community/career";
+import OurCoreValues from "./pages/community/our-core-values";
+import WorkCulture from "./pages/community/work-culture";
+import FAQ from "./pages/community/faq";
+import Blogs from "./components/home/Blogs";
+import PrivacyPolicy from "./components/PrivacyPolicy";
 
 function App() {
   return (
@@ -41,12 +41,15 @@ function App() {
             <Route path="/our-story/about" element={<AboutUs />} />
             <Route path="/our-story/mission" element={<OurMission />} />
             <Route path="/apply" element={<ApplyButton />} />
-            <Route path="/our-story/team" element={<ContactUs />} />
+            <Route path="/ContactUS" element={<ContactUs />} />
             <Route path="/loans" element={<Loans />} />
             <Route path="/community" element={<Community />} />
-            
+
             <Route path="/community/career" element={<Career />} />
-            <Route path="/community/our-core-values" element={<OurCoreValues />} />
+            <Route
+              path="/community/our-core-values"
+              element={<OurCoreValues />}
+            />
             <Route path="/community/work-culture" element={<WorkCulture />} />
             <Route path="/community/faq" element={<FAQ />} />
             <Route path="/loans/personal" element={<PersonalLoan />} />
@@ -60,10 +63,11 @@ function App() {
         </main>
         <Footer />
         <ContactButtons />
-        <ScrollToTop /> {/* ✅ Add this at the bottom so it's on top of everything */}
+        <ScrollToTop />{" "}
+        {/* ✅ Add this at the bottom so it's on top of everything */}
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
