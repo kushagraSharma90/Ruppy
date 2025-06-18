2e98223c2ac4c9770542d29d1d160004f4fe0609
NITYA-C0 <154868587+NITYA-C0@users.noreply.github.com>
2025-06-18 17:12:24 +0530

    changes

--- src/pages/community/Blogs.jsx
+++ src/pages/community/Blogs.jsx
@@ -51,7 +51,7 @@ const blogPosts = [
   {
     id: 4,
     title: "Key Challenges Faced by Used Car Dealers and How RupeeDot Is Solving Them?",
-    excerpt: "A comprehensive guide to understanding how home loan interest rates work and how to get the best deal.",
+    excerpt: "Indian used car dealers particularly small and medium-sized dealerships work in a highspeed market but are often hindered by slow, inefficient financing procedures. One of the largest challenges they encounter is slow loan approvals, which not only delays the sales process but also dissolves customer confidence. Customers today anticipate near-instant approval, and extended waiting times frequently result in drop-offs or lost business.",
     image: blog4,
     // category: "Home Loans",
     views: 325,
@@ -64,7 +64,8 @@ const blogPosts = [
   {
     id: 5,
     title: "What Determines Your Loan Eligibility in India?",
-    excerpt: "Practical tips to boost your credit score quickly and improve your financial standing.",
+    excerpt: "Understanding what impacts your loan qualification enables you to prepare and apply confidently. Keep your credit rating high, have a stable income, have control over your current debts, and be open about your loan requirements. This makes it easier and quicker for you.",
+ 
     image: blog5,
     // category: "Credit Score",
     views: 892,
